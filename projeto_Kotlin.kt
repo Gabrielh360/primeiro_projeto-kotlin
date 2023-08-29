ef345683772276488a766930941006f9923f5d4c
Gabrielh360 <banedragon360@gmail.com>
2023-08-29 02:29:01 -0300

    A

--- projeto_Kotlin.kt
+++ projeto_Kotlin.kt
@@ -10,66 +10,70 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     
     fun matricular(usuario: Usuario) {
         if (inscritos.contains(usuario)) {
-            inscritos.remove(usuario)
-            println("[LOG] ${usuario.aluno} já está inscrito(a) nesta formação.")
+           	throw IllegalArgumentException("${usuario.aluno} já está inscrito(a) nesta formação.")
         } else {
-            inscritos.add(usuario)
-            println("[LOG] ${usuario.aluno} foi inscrito(a) em ${this.nome}.")
+            inscritos.add(usuario) 
+            log("${usuario.aluno} foi inscrito(a) em $nome.")
         }
-        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    }
+    
+    private fun log(message: String) {
+        println("[LOG] $message")
     }
     
 }
 
+
 fun main() {
-    val usuario1 = Usuario("Gabriel", 21)
-    val usuario2 = Usuario("Lucas", 24)
-    val usuario3 = Usuario("João", 19)
-    val usuario4 = Usuario("Ana", 33)
-    val usuario5 = Usuario("Rafael", 20)
-    val usuario6 = Usuario("Jessica", 27)
+    val usuarios = listOf(
+    	Usuario("Gabriel", 21),
+    	Usuario("Lucas", 24),
+    	Usuario("João", 19),
+    	Usuario("Ana", 33),
+    	Usuario("Rafael", 20),
+    	Usuario("Jessica", 27)
+    )
     
-    val conteudo1 = ConteudoEducacional("Santander Bootcamp 2023 - Mobile Android com Kotlin", 98, Nivel.INTERMEDIARIO)
-    val conteudo2 = ConteudoEducacional("Santander Bootcamp 2023 - Mobile iOS com Swift", 89, Nivel.INTERMEDIARIO)
-    val conteudo3 = ConteudoEducacional("Santander Bootcamp 2023 - Mobile com Flutter", 89, Nivel.INTERMEDIARIO)
-    val conteudo4 = ConteudoEducacional("Santander Bootcamp 2023 - Ciência de Dados com Python", 89, Nivel.INTERMEDIARIO)
-    val conteudo5 = ConteudoEducacional("Santander Bootcamp 2023 - Backend Java", 88, Nivel.INTERMEDIARIO)
-    val conteudo6 = ConteudoEducacional("Santander Bootcamp 2023 - Fullstack Java+Angular", 115, Nivel.INTERMEDIARIO)
     
-    val formacao1 = Formacao("Mobile Android com Kotlin", listOf(conteudo1))
-    val formacao2 = Formacao("Mobile iOS com Swift", listOf(conteudo2))    
-    val formacao3 = Formacao("Mobile com Flutter", listOf(conteudo3))
-    val formacao4 = Formacao("Ciência de Dados com Python", listOf(conteudo4))
-    val formacao5 = Formacao("Backend Java", listOf(conteudo5))
-    val formacao6 = Formacao("Fullstack Java+Angula", listOf(conteudo6))
+    val conteudos = listOf(
+    	ConteudoEducacional("Santander Bootcamp 2023 - Mobile Android com Kotlin", 98, Nivel.INTERMEDIARIO),
+    	ConteudoEducacional("Santander Bootcamp 2023 - Mobile iOS com Swift", 89, Nivel.INTERMEDIARIO),
+    	ConteudoEducacional("Santander Bootcamp 2023 - Mobile com Flutter", 89, Nivel.INTERMEDIARIO),
+    	ConteudoEducacional("Santander Bootcamp 2023 - Ciência de Dados com Python", 89, Nivel.INTERMEDIARIO),
+    	ConteudoEducacional("Santander Bootcamp 2023 - Backend Java", 88, Nivel.INTERMEDIARIO),
+    	ConteudoEducacional("Santander Bootcamp 2023 - Fullstack Java+Angular", 115, Nivel.INTERMEDIARIO)
+    )
     
-    formacao1.matricular(usuario1)
-    formacao2.matricular(usuario2)
-    formacao3.matricular(usuario3)
-    formacao4.matricular(usuario4)
-    formacao5.matricular(usuario5)
-    formacao6.matricular(usuario6)
+    
+    val formacoes = listOf(
+    	Formacao("Mobile Android com Kotlin", listOf(conteudos[0])),
+    	Formacao("Mobile iOS com Swift", listOf(conteudos[1])),    
+    	Formacao("Mobile com Flutter", listOf(conteudos[2])),
+    	Formacao("Ciência de Dados com Python", listOf(conteudos[3])),
+    	Formacao("Backend Java", listOf(conteudos[4])),
+    	Formacao("Fullstack Java+Angula", listOf(conteudos[5]))
+    )
+    
+    
+    formacoes[0].matricular(usuarios[0])
+    formacoes[1].matricular(usuarios[1])
+    formacoes[2].matricular(usuarios[2])
+    formacoes[3].matricular(usuarios[3])
+    formacoes[4].matricular(usuarios[4])
+    formacoes[5].matricular(usuarios[5])
+    
+    
+    println("\n[INSCRIÇÕES]")
+    for (formacao in formacoes) {
+        println("Inscritos na formção ${formacao.nome}: ${formacao.inscritos}")
+    }
     
     println()
     
-    println("[INSCRIÇÕES]")
-    println("Inscritos na formção ${formacao1.nome}: ${formacao1.inscritos}")
-    println("Inscritos na formção ${formacao2.nome}: ${formacao2.inscritos}")
-    println("Inscritos na formção ${formacao3.nome}: ${formacao3.inscritos}")
-    println("Inscritos na formção ${formacao4.nome}: ${formacao4.inscritos}")
-    println("Inscritos na formção ${formacao5.nome}: ${formacao5.inscritos}")
-    println("Inscritos na formção ${formacao6.nome}: ${formacao6.inscritos}")
+    for (conteudo in conteudos) {
+        println("[CONTEUDO INFO] $conteudo")
+    }
     
-    println()
-    
-    println("[CONTEUDO INFO] ${conteudo1}")
-    println("[CONTEUDO INFO] ${conteudo2}")
-    println("[CONTEUDO INFO] ${conteudo3}")
-    println("[CONTEUDO INFO] ${conteudo4}")
-    println("[CONTEUDO INFO] ${conteudo5}")
-    println("[CONTEUDO INFO] ${conteudo6}")
-    
-    
-    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
 }
